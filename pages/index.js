d88b172cf17239fcefe10ee5bbd06937c34eb8f2
Anderson Santo <kalangoti@gmail.com>
2023-04-03 20:46:45 -0300

    feat: Add first page Home with next

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <>
+      <h1>Minha querida esposa Carla... EU TE AMO MUITO!!!</h1>
+      <h2>Amo mais ainda a nossa família</h2>
+    </>
+  );
+}
+
+export default Home;
